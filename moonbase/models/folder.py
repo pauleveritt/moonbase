2d6b0e7049efdb44d4149a6244e2e90da032244a
Paul Everitt <paul@agendaless.com>
2015-06-17 12:45:34 -0400

    Don't need to json.

--- moonbase/models/folder.py
+++ moonbase/models/folder.py
@@ -12,10 +12,3 @@ class Folder(Node):
     __tablename__ = 'folder'
     id = Column(Integer, ForeignKey('node.id'), primary_key=True)
     title = Column(Text)
-
-    def __json__(self, request):
-        return dict(
-            id=self.id,
-            title=self.title,
-            resourceType='Folder'
-        )
